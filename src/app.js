7647388fbd0f1865f8afd17d79bd116c5a5c7278
algorithmsrock <algorithmsrock@gmail.com>
2018-03-09 10:38:31 -0800

    Get request previous bit coin price working

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@
  const server = express();
  const PORT = 3030;
  const CURRENT_PRICE = "https://api.coindesk.com/v1/bpi/currentprice.json";
+ const PREVIOUS_PRICE = "https://api.coindesk.com/v1/bpi/historical/close.json";
  
   server.get('/current', (req, res) => {
    fetch(CURRENT_PRICE)
@@ -18,7 +19,20 @@
           res.send(`There was an error getting current bitcoin price: ${err}`)
         })
     })
- 
+
+   server.get('/previous', (req, res) => {
+			 fetch(PREVIOUS_PRICE)
+			   .then(res => res.json())
+				 .then(previous => {
+					  res.status(200)
+						res.send(previous)
+          })
+				 .catch(err => {
+						 res.status(422)
+						 res.send(`There was an error getting previous bitcoin price: ${err}`)
+          })
+			})
+
   server.listen(PORT, (err) => {
       if (err) {
          console.log(`There was an error: ${err}`);
